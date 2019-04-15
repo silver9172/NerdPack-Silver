5f317a145ef959d587f5c0947c165261948466e1
silver9172 <chrisfodge@me.com>
2019-04-15 09:26:04 -0400

    Hunter Updates

--- rotations/hunter/beast_mastery.lua
+++ rotations/hunter/beast_mastery.lua
@@ -14,16 +14,25 @@ local survival = {
 
 }
 
+local utility = {
+	{ 'Tar Trap', 'inRange.spell(Cobra Shot) && infront(player) && !bosscheck', 'target.ground'},
+
+}
+
 local misdirection = {
-	{ 'Misdirection', '!player.buff & !focus.exists & pet.exists', 'pet'},
-	{ 'Misdirection', '!player.buff & focus.exists', 'focus'},
-	{ 'Misdirection', '!player.buff & tank.exists & threat > 95', 'tank'},
+	{ 'Misdirection', '!player.buff && focus.exists', 'focus'},
+	{ 'Misdirection', '!player.buff && tank.exists', 'tank'},
+	{ 'Misdirection', '!player.buff && !focus.exists && pet.exists', 'pet'},
 }
 
 local petAbilities = {
+	{ '*/petattack', 'inRange.spell(Cobra Shot) && infront(player) && targettimeout(petattack, 20)', 'target'},
 	{ 'Bite', 'player.spell.exists', 'target'},
 	{ 'Claw', 'player.spell.exists', 'target'},
 	{ 'Smack', 'player.spell.exists', 'target'},
+
+	{ 'Mend Pet', 'health < 75 && !buff', 'pet'},
+	{ 'Revive Pet', 'dead', 'pet'},
 }
 
 local cooldowns = {
@@ -32,7 +41,7 @@ local cooldowns = {
 }
 
 local interrupts = {
-	{'Counter Shot'},
+	{'Counter Shot', 'inRange.spell && infront(player) && interruptAt(35)', { 'target', 'enemies'}},
 }
 
 local keybinds = {
@@ -42,55 +51,65 @@ local keybinds = {
 
 local rotation = {
 	-- actions=auto_shot
-	{ '*/startattack', 'distance <= 40 & targettimeout(autoshot, 10)', 'target'},
+	{ '*/startattack', 'inRange.spell(Cobra Shot) && infront(player) && targettimeout(autoshot, 10)', 'target'},
 	-- actions+=/use_items
 	-- actions+=/berserking,if=cooldown.bestial_wrath.remains>30
+	{ 'Berserking', 'inRange.spell(Cobra Shot) && infront(player) && spell(Bestial Wrath).cooldown > 30', 'target'},
 	-- actions+=/blood_fury,if=cooldown.bestial_wrath.remains>30
+	{ 'Blood Fury', 'inRange.spell(Cobra Shot) && infront(player) && spell(Bestial Wrath).cooldown > 30', 'target'},
 	-- actions+=/ancestral_call,if=cooldown.bestial_wrath.remains>30
+	{ 'Ancestral Call', 'inRange.spell(Cobra Shot) && infront(player) && spell(Bestial Wrath).cooldown > 30', 'target'},
 	-- actions+=/fireblood,if=cooldown.bestial_wrath.remains>30
-	-- actions+=/lights_judgmentm
-	-- actions+=/potion,if=buff.bestial_wrath.up&buff.aspect_of_the_wild.up
-	-- actions+=/barbed_shot,if=pet.cat.buff.frenzy.up&pet.cat.buff.frenzy.remains<=gcd.max
-	{ 'Barbed Shot', 'pet.buff(Frenzy) & pet.buff(Frenzy).duration <= gcd.max'},
-	-- actions+=/a_murder_of_crows
-	{ 'A Murder of Crows'},
+	{ 'Fireblood', 'inRange.spell(Cobra Shot) && infront(player) && spell(Bestial Wrath).cooldown > 30', 'target'},
+	-- actions+=/potion,if=buff.bestial_wrath.up&buff.aspect_of_the_wild.up&(target.health.pct<35|!talent.killer_instinct.enabled)|target.time_to_die<25
+	-- actions+=/barbed_shot,if=pet.cat.buff.frenzy.up&pet.cat.buff.frenzy.remains<=gcd.max|full_recharge_time<gcd.max&cooldown.bestial_wrath.remains
+	{ 'Barbed Shot', 'inRange.spell(Cobra Shot) && infront(player) && pet.buff(Frenzy) && pet.buff(Frenzy).duration <= gcd', 'target'},
+	-- actions+=/lights_judgment
 	-- actions+=/spitting_cobra
-	{ 'Spitting Cobra'},
-	-- actions+=/stampede,if=buff.bestial_wrath.up|cooldown.bestial_wrath.remains<gcd|target.time_to_die<15
-	{ 'Stampede', 'player.buff(Bestial Wrath) || player.spell(Bestial Wrath).cooldown < gcd || ttd < 15', 'target'},
+	{ 'Spitting Cobra', 'inRange.spell && infront(player)', 'target'},
 	-- actions+=/aspect_of_the_wild
-	-- actions+=/bestial_wrath,if=!buff.bestial_wrath.up
-	{ 'Bestial Wrath', '!player.buff(Bestial Wrath)'},
-	-- actions+=/multishot,if=spell_targets>2&(pet.cat.buff.beast_cleave.remains<gcd.max|pet.cat.buff.beast_cleave.down)
-	{ 'Multi-shot', 'area(8).enemies > 2 & { player.buff(Beast Cleave).duration < gcd || !player.buff(Beast Cleave)}', 'target'},
-	-- actions+=/chimaera_shot
-	{ 'Chimaera Shot'},
+	{ 'Aspect of the Wild', 'inRange.spell(Cobra Shot) && infront(player)', 'target'},
+	-- actions+=/a_murder_of_crows,if=active_enemies=1
+	{ 'A Murder of Crows', 'inRange.spell && infront(player) && area(15).enemies = 1', 'target'},
+	-- actions+=/stampede,if=buff.aspect_of_the_wild.up&buff.bestial_wrath.up|target.time_to_die<15
+	-- actions+=/multishot,if=spell_targets>2&gcd.max-pet.cat.buff.beast_cleave.remains>0.25
+	{ 'Multi-Shot', 'inRange.spell && infront(player) && area(8).enemies > 2 && gcd - pet.buff(Beast Cleave).duration > 0.25', 'target'},
+	-- actions+=/bestial_wrath,if=cooldown.aspect_of_the_wild.remains>20|target.time_to_die<15
+	{ 'Bestial Wrath', 'inRange.spell(Cobra Shot) && infront(player) && { spell(Aspect of the Wild).cooldown > 20 || ttd < 15 }', 'target'},
+	-- actions+=/barrage,if=active_enemies>1
+	{ 'Barrage', 'inRange.spell(Cobra Shot) && infront(player) && area(8).enemies > 1', 'target'},
+	-- actions+=/chimaera_shot,if=spell_targets>1
+	{ 'Chimaera Shot', 'inRange.spell && infront(player) && area(8).enemies > 1', 'target'},
+	-- actions+=/multishot,if=spell_targets>1&gcd.max-pet.cat.buff.beast_cleave.remains>0.25
+	{ 'Multi-Shot', 'inRange.spell && infront(player) && area(8).enemies > 1 && gcd - pet.buff(Beast Cleave).duration > 0.25', 'target'},
 	-- actions+=/kill_command
-	{ 'Kill Command'},
+	{ 'Kill Command', 'inRange.spell && infront(player)', 'target'},
+	-- actions+=/chimaera_shot
+	{ 'Chimaera Shot', 'inRange.spell && infront(player)', 'target'},
+	-- actions+=/a_murder_of_crows
+	{ 'A Murder of Crows', 'inRange.spell && infront(player)', 'target'},
 	-- actions+=/dire_beast
-	{ 'Dire Beast'},
-	-- actions+=/barbed_shot,if=pet.cat.buff.frenzy.down&charges_fractional>1.4|full_recharge_time<gcd.max|target.time_to_die<9
-	{ 'Barbed Shot', '!pet.buff(Frenzy) & player.spell.charges > 1.4 || player.spell.recharge < gcd || ttd < 9', 'target'},
-	-- actions+=/multishot,if=spell_targets>1&(pet.cat.buff.beast_cleave.remains<gcd.max|pet.cat.buff.beast_cleave.down)
-	{ 'Multi-shot', 'area(8).enemies > 1 & { player.buff(Beast Cleave).duration < gcd || !player.buff(Beast Cleave).duration}', 'target'},
-	-- actions+=/cobra_shot,if=(active_enemies<2|cooldown.kill_command.remains>focus.time_to_max)&(buff.bestial_wrath.up&active_enemies>1|cooldown.kill_command.remains>1+gcd&cooldown.bestial_wrath.remains>focus.time_to_max|focus-cost+focus.regen*(cooldown.kill_command.remains-1)>action.kill_command.cost)
-	{ 'Cobra Shot', '{ area(8).enemies < 2 || player.spell(Kill Command).cooldown > focus.time_to_max} & { player.buff(Bestial Wrath) & area(8).enemies > 1 || player.spell(Kill Command).cooldown > 1 + gcd & player.spell(Bestial Wrath).cooldown > focus.time_to_max || player.focus - 35 + focus.regen * { player.spell(Kill Command).cooldown - 1} > 30}', 'target'},
-
--- /cast !auto shot
--- /cast Cobra Shot
--- /script UIErrorsFrame:Clear();
+	{ 'Dire Beast', 'inRange.spell && infront(player)', 'target'},
+	-- actions+=/barbed_shot,if=pet.cat.buff.frenzy.down&(charges_fractional>1.8|buff.bestial_wrath.up)|cooldown.aspect_of_the_wild.remains<6&azerite.primal_instincts.enabled|target.time_to_die<9
+ 	{ 'Barbed Shot', 'inRange.spell && infront(player) && { !pet.buff(Frenzy) && player.spell.charges > 1.4 || player.spell.recharge < gcd || ttd < 9}', 'target'},
+	-- actions+=/barrage
+	{ 'Barrage', 'inRange.spell(Cobra Shot) && infront(player)', 'target'},
+	-- actions+=/cobra_shot,if=(active_enemies<2|cooldown.kill_command.remains>focus.time_to_max)&(focus-cost+focus.regen*(cooldown.kill_command.remains-1)>action.kill_command.cost|cooldown.kill_command.remains>1+gcd)&cooldown.kill_command.remains>1
+	{ 'Cobra Shot', 'inRange.spell && infront(player) && {{ target.area(15).enemies < 2 || spell(Kill Command).cooldown > focus.time_to_max } && { focus - 35 + power.regen * { spell(Kill Command).cooldown - 1} > 30 || spell(Kill Command).cooldown > 1 + gcd} && spell(Kill Command).cooldown > 1}'},
+	-- actions+=/arcane_torrent
 }
 
 local inCombat = {
 	{ misdirection},
-	{ interrupts, 'target.interruptAt(35)'},
+	{ interrupts},
 	{ petAbilities},
+	{ utility},
 	{ rotation},
 }
 
 local outCombat = {
 	{ Keybinds},
-	{ 'Dismiss Pet', 'pet.exists & toggle(PetSummon) & !player.buff(Feign Death)'},
+	{ 'Dismiss Pet', 'pet.exists && toggle(PetSummon) && !player.buff(Feign Death)'},
 }
 
 NeP.CR:Add(253, {
